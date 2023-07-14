75e6c561dfa83cd6d159d1c476d8b07434f9c2be
Mateus Silva <trabalhomateusjs521@gmail.com>
2023-07-14 16:04:06 -0300

    Add global stylesheet to project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap'
+import './assets/css/style.css'
 
 createApp(App).use(router).mount('#app')
