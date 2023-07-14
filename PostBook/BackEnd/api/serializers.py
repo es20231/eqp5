d37c88d047397b407842ee3c9a53e486d856f023
GleidesonFr <gleidesonfreitas015@gmail.com>
2023-07-14 13:14:08 -0300

    Updating branch main

--- PostBook/BackEnd/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from api.models import Record
-
-class RecordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Record
-        fields = "__all__"
--- PostBook/BackEnd/api/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# Todas as views devem seguir um padrão
-# implementadado no tutorial
