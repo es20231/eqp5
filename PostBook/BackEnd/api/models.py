9ed0e47eca238418b4360903f09d9cca3c049f25
GleidesonFr <gleidesonfreitas015@gmail.com>
2023-07-14 09:43:04 -0300

    Adicionando ambiente configurado do projeto

---
+++ PostBook/BackEnd/api/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+#Follow the tutorial's standard to develop your
+# model
+
---
+++ PostBook/BackEnd/api/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Todas as views devem seguir um padrão
+# implementadado no tutorial
