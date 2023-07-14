0897da70c1e87c42d7100266b2741dd146ce329d
Mateus Silva <trabalhomateusjs521@gmail.com>
2023-07-14 16:07:27 -0300

    Created the SignIn screen template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,16 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+  <div class="app-container">
+    <SignIn />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SignIn from "./components/SignIn.vue";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    SignIn,
+  },
+};
+</script>
