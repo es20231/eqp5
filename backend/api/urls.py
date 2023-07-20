a495a2575fdad4e9604265fc3beaf69e97c60784
GleidesonFr <gleidesonfreitas015@gmail.com>
2023-07-19 21:51:47 -0300

    Backend Django Updated

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,1 @@
-from api import views
-from django.urls import path
-
-app_name = "api"
-
-urlpatterns = [
-    path("sign-up/", views.UsersAPIView.as_view(), name="sign-up-users"),
-]
+#Create your url here.
