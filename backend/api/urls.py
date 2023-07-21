1dc68752e66f9473b03f2a6a3920ebf1bb7e8ef5
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-21 15:09:34 -0300

    feat: definindo as urls para o UserAPIView.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-
+from api import views
 
 app_name = "api"
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('users/', views.UserAPIView.as_view(), name="user_api_list"),
+    path('users/<int:id>/', views.UserAPIView.as_view(), name="user_api_detail"),   
 ]
