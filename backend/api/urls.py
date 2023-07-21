306bb0ce86553753e5898e9add206dbc10fd625a
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-21 15:11:26 -0300

    feat: usando o CustomJWTSerializer como serializer_class para permitir obter o token usando o email como campo de username.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from api.serializers import CustomJWTSerializer
 from api import views
 
 app_name = "api"
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(serializer_class=CustomJWTSerializer), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('users/', views.UserAPIView.as_view(), name="user_api_list"),
