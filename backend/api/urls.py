ac41d22e69f6fbc8484f0076893fd31659548b1d
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-22 18:26:01 -0300

    feat: configurando a url para servir os arquivos estáticos.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from api.serializers import CustomJWTSerializer
 from api import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 app_name = "api"
 
@@ -14,3 +17,6 @@ urlpatterns = [
     path('profiles/', views.ProfileAPIView.as_view(), name="profile_api_list"),
     path('profiles/<int:id>/', views.ProfileAPIView.as_view(), name="profile_api_detail"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
