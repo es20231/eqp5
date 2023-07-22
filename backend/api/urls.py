12ad8695a72dd88d5782185ed6daf10c1ee3b0ef
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-22 16:24:50 -0300

    update: alterando os urls da ProfileAPIView.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('users/', views.UserAPIView.as_view(), name="user_api_list"),
     path('users/<int:id>/', views.UserAPIView.as_view(), name="user_api_detail"),
-    path('users/profiles/', views.ProfileAPIView.as_view(), name="profile_api_list"),
-    path('users/<int:id>/profiles/', views.ProfileAPIView.as_view(), name="profile_api_detail"),
+    path('profiles/', views.ProfileAPIView.as_view(), name="profile_api_list"),
+    path('profiles/<int:id>/', views.ProfileAPIView.as_view(), name="profile_api_detail"),
 ]
