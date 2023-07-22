cb7a92dabee2b7714ee5bd3598e377989e9eb3cc
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-22 14:42:33 -0300

    feat: criando as urls para a ProfileAPIView.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('users/', views.UserAPIView.as_view(), name="user_api_list"),
-    path('users/<int:id>/', views.UserAPIView.as_view(), name="user_api_detail"),   
+    path('users/<int:id>/', views.UserAPIView.as_view(), name="user_api_detail"),
+    path('users/profiles/', views.ProfileAPIView.as_view(), name="profile_api_list"),
+    path('users/<int:id>/profiles/', views.ProfileAPIView.as_view(), name="profile_api_detail"),
 ]
