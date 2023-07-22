86702f0ef671a75d7aadef337a96a9d652ec1d81
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-22 14:39:35 -0300

    feat: método get no ProfileAPIView para listar ou detalhar um perfil.

--- backend/api/views/profile.py
+++ backend/api/views/profile.py
@@ -14,3 +14,21 @@ class ProfileAPIView(APIView):
 
     def get_queryset(self):
         return Profile.objects.all()
+    
+    def get(self, request: HttpRequest, id: int) -> Response:
+        try:
+            results = self.get_queryset().get(id=id)
+            if not id:
+                params = request.query_params.copy()
+                params = {key: params[key] for key in params.keys()}
+                per_page = int(params.pop("per_page", 10))
+                page = int(params.pop("page", 1))
+                results = paginate_response(
+                    items=self.get_queryset(),
+                    serializer=ProfileSerializer,
+                    per_page=per_page,
+                    page=page
+                )
+            return Response(data=results, status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response(data={"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
