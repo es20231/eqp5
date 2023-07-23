910426f1a668afc23ac57b3b3b0c1fe431a02a73
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-23 00:17:32 -0300

    feat:  método get_user_profile na cbv PostAPIView, para obter o perfil do usuário autenticado.

--- backend/api/views/post.py
+++ backend/api/views/post.py
@@ -1,4 +1,4 @@
-from api.models import Post
+from api.models import Post, Profile
 from api.serializers import PostSerializer
 from api.utils import paginate_response
 from rest_framework import status
@@ -10,4 +10,8 @@ from rest_framework.views import APIView
 
 class PostAPIView(APIView):
     permission_classes = [IsAuthenticated,]
+
+    def get_user_profile(self) -> Profile:
+        user_id = self.request.user.id
+        return Profile.objects.all().get(user__id=user_id)
     
