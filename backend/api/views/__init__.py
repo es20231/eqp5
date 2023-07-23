d488b8e7398ad430faf06e16d1b8029d40f4bd50
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-22 22:04:38 -0300

    feat: LogoutAPIView.

--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -2,3 +2,4 @@ from .user import UserAPIView
 from .profile import ProfileAPIView
 from .post import PostAPIView
 from .forgot_password import ForgotPasswordAPIView
+from .logout import LogoutAPIView
