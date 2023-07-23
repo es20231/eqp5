5dceb167111c1c07cd3f0bac33d713fe3efb048d
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-23 01:20:11 -0300

    fet: método get na cbv PostAPIVIew.

--- backend/api/views/post.py
+++ backend/api/views/post.py
@@ -38,3 +38,73 @@ class PostAPIView(APIView):
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         except Exception as error:
             return Response(data={"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
+        
+    def get(self, request: HttpRequest, id:int = None) -> Response:
+        try:
+            if id:
+                filters = {
+                    "is_posted": eval(request.query_params.pop("is_posted", "True").title())
+                }
+                object = self.get_object(**filters)
+                results = PostSerializer(instance=object).data
+            elif not id:
+                params = request.query_params.copy()
+                params = {key: params[key] for key in params.keys()}
+
+                per_page = int(params.pop("per_page", 10))
+                page = int(params.pop("page", 1))
+                order_by = params.pop("order_by", "created_at")
+                sort_direction = "-" if int(params.pop("sort_direction", -1)) == -1 else ""
+                ordering = f"{sort_direction}{order_by}"
+
+                filters = {}
+
+                if "is_posted" in params:
+                    filters["is_posted"] = eval(params.pop("is_posted").title())
+                if "created_at" in params:
+                    filters
+                if "profile_id" in params:
+                    filters["profile__id"] = params.pop("profile_id")
+                if "user_id" in params:
+                    filters["profile__user__id"] = params.pop("user_id")
+                if "username" in params:
+                    filters["profile__user__username"] = params.pop("username")
+                if "my_posts" in params:
+                    filters["profile__user__id"] = self.request.user.id
+                
+                objects = self.get_queryset().filter(**filters)
+
+                results = paginate_response(
+                    items=objects,
+                    serializer=PostSerializer,
+                    per_page=per_page,
+                    page=page,
+                    context= {"request": request}
+                )
+
+            return Response(data=results, status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response(data={"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
+        
+    def patch(self, request: HttpRequest, id: int = None) -> Response:
+        try:
+            filters = {
+                "is_posted": eval(request.query_params.pop("is_posted", "True").title())
+            }
+            post = self.get_object(**filters)
+            serializer = PostSerializer(instance=post, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response(data={"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request: HttpRequest, id: int = None) -> Response:
+        try:
+            filters = {
+                "is_posted": eval(request.query_params.pop("is_posted", "True").title())
+            }
+            post = self.get_object(**filters).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as error:
+            return Response(data={"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
