bed44153f89d35f16f51d940dc14093ce8dc667c
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-22 22:08:02 -0300

    feat: url para logout da cbv LogoutAPIView.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(serializer_class=CustomJWTSerializer), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('logout/', views.LogoutAPIView.as_view(), name="api_logout"),
     path('users/', views.UserAPIView.as_view(), name="user_api_list"),
     path('users/<int:id>/', views.UserAPIView.as_view(), name="user_api_detail"),
-    path('users/forgot-password/', views.ForgotPasswordAPIView.as_view(), name="forgot_password"),
     path('profiles/', views.ProfileAPIView.as_view(), name="profile_api_list"),
     path('profiles/<int:id>/', views.ProfileAPIView.as_view(), name="profile_api_detail"),
 ]
