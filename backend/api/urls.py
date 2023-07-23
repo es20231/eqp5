b058bac1f2ec0fb7a60ab59d83c30a2b3faf1025
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-22 21:21:28 -0300

    update: alterando os nomes das urls relacionadas aos tokens.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,9 +9,9 @@ from django.conf.urls.static import static
 app_name = "api"
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(serializer_class=CustomJWTSerializer), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('login/', TokenObtainPairView.as_view(serializer_class=CustomJWTSerializer), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('users/', views.UserAPIView.as_view(), name="user_api_list"),
     path('users/<int:id>/', views.UserAPIView.as_view(), name="user_api_detail"),
     path('users/forgot-password/', views.ForgotPasswordAPIView.as_view(), name="forgot_password"),
