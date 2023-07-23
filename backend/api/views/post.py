973b66a2beb60da6f48768d7618572cc2979c13d
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-23 01:17:27 -0300

    feat: método get_object na cbv PostAPIView.

--- backend/api/views/post.py
+++ backend/api/views/post.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import HttpRequest
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from api.permissions import IsPostOwner
 
 
 class PostAPIView(APIView):
@@ -18,6 +19,12 @@ class PostAPIView(APIView):
     def get_queryset(self):
         return Post.objects.all()
 
+    def get_object(self, **kwargs) -> Post:
+        id = self.kwargs.get("id")
+        post = self.get_queryset().get(id=id, **kwargs)
+        self.check_object_permissions(self.request, post)
+        return post
+    
     def post(self, request: HttpRequest) -> Post:
         try:
             serializer = PostSerializer(data=request.data, context={"request": request})
