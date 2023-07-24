2f82400e3019bca79f3014b0898d9158899b0389
acorreiacruz <antoniocruzjunior@outlook.com>
2023-07-24 11:36:28 -0300

    feat: alterando o método save do model Post para permitir imagens de no máximo 10mb.

--- backend/api/models/post.py
+++ backend/api/models/post.py
@@ -1,5 +1,9 @@
-from django.db import models
+from io import BytesIO
+from typing import Iterable, Optional
+
 from api.models import Profile
+from django.db import models
+from PIL import Image
 
 
 class Post(models.Model):
@@ -9,6 +13,17 @@ class Post(models.Model):
     description = models.CharField(max_length=1000, null=True, blank=True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="posts")
     is_posted = models.BooleanField(default=False, null=False)
+
+    def save(self, *args, **kwargs) -> None:
+        if self.image:
+            img = Image.open(self.image)
+            img_io = BytesIO()
+            img.save(img_io, format=img.format)
+            img_size = img_io.tell() 
+            size_limit = 10 * 1024 * 1024
+            if img_size > size_limit:
+                 raise ValueError("A imagem excede o tamanho máximo permitido (10 MB).")
+        return super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Post"
