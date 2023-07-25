83f64fc56b7aa8ba14110cc53f12dcaa3f2b39ed
Lucas <lucasdavid5882@gmail.com>
2023-07-24 21:04:20 -0300

    test:primeiros testes api

---
+++ backend/api/tests/test_user_api_view.py
@@ -0,0 +1,54 @@
+from api.views import UserAPIView
+from rest_framework.test import APITestCase
+from django.urls import resolve, reverse
+from api.tests.utils import APITestBase
+
+
+class UserAPIViewTest(APITestCase,APITestBase):
+    def test_if_post_url_is_correct(self):
+        endpoint = reverse("api:user_api_list")
+        self.assertEqual("/api/users/", endpoint)
+
+    def test_if_post_returns_status_code_201(self):
+        url = reverse("api:user_api_list")
+        response = self.client.post(
+            path=url,
+            data={**self.get_user_data()}
+        )
+        self.assertEqual(response.status_code, 201)
+
+    def test_if_post_creates_a_user(self):
+        url = reverse("api:user_api_list")
+        user_data = self.get_user_data()
+        response = self.client.post(
+            path=url,
+            data={**user_data}
+        )
+        user_created_data = response.data
+        self.assertEqual(user_created_data.get("username"), user_data.get("username"))
+        self.assertEqual(user_created_data.get("full_name"), user_data.get("full_name"))
+        self.assertEqual(user_created_data.get("email"), user_data.get("email"))
+
+    def test_if_unregister_user_can_login(self):
+        url = reverse("api:token_obtain_pair")
+        response = self.client.post(
+            path=url,
+            data={
+                "email": "user@email.com",
+                "password": "P@ssword1234"
+            }
+        )
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data.get("detail"), "No active account found with the given credentials")
+    
+    def test_if_registered_user_can_login(self):
+        user = self.create_user()
+        url = reverse("api:token_obtain_pair")
+        response = self.client.post(
+            path=url,
+            data={
+                "email": "jhondoe@email.com",
+                "password": "qaz12345"
+            }
+        )
+        self.assertEqual(response.status_code, 200)
---
+++ backend/api/tests/utils/api_test_base.py
@@ -0,0 +1,75 @@
+from django.urls import reverse
+from api.models import User
+from typing import List
+
+class APITestBase:
+    def _init_(self) ->  None:
+        self.views_urls = {
+            "user": {
+                "get": {
+                    "list": "/api/users/",
+                    "detail": "/api/users/"
+                },
+                "post": {
+                    "list": "/api/users/"
+                },
+                "patch": {
+                    "detail": "/api/users/"
+                }
+            },
+            "post": {},
+            "profile": {}
+        }
+    def create_user(
+        self,
+        full_name="Jhon Doe",
+        username="jhondoe",
+        email="jhondoe@email.com",
+        password="qaz12345",
+    ) -> User:
+        return User.objects.create_user(
+            full_name=full_name,
+            username=username,
+            email=email,
+            password=password
+        )
+    
+    def create_user_set(self, quant: int = 10) -> List[User]:
+        users_set = []
+        for i in range(quant + 1):
+            user_info = {
+                    "username": f"user{i + 1}",
+                    "full_name": f"Full Name User{i + 1}",
+                    "email": f"user{i + 1}@gmail.com",
+                    "password": "qaz12345",
+                }
+            user = self.create_user(**user_info)
+            users_set.append(user)
+        return users_set
+    
+    def get_user_data(self):
+        return {
+            "username": "jhondoe",
+            "email": "jhondoe@email.com",
+            "password": "qaz12345",
+            "full_name": "Jhon Doe",
+        }
+    
+    def get_tokens(self):
+        user = self.create_user(**self.get_user_data())
+        response = self.client.post(
+            path=reverse("api:token_obtain_pair"),
+            data={
+                "email": "jhondoe@email.com",
+                "password": "Qaz123456"
+            }
+        )
+        return {
+            "access": response.data.get("access"),
+            "refresh": response.data.get("refresh"),
+            "user": user
+        }
+    
+    def get_api_url(method: str = "get", view_name: str = "user", action: str = "list") -> str:
+        if view_name == "user":
+            ...
