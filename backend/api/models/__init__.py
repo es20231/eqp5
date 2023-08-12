e1b41ee9a4d5ab32edb5c085bd38cbcb6d8ece5c
acorreiacruz <antoniocruzjunior@outlook.com>
2023-08-12 13:16:21 -0300

    feat: model RemarkReaction que irá salvar os likes e os dislikes de um comentário.

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -2,3 +2,4 @@ from .user import User
 from .profile import Profile
 from .post import Post
 from .remark import Remark
+from .remark_reaction import RemarkReaction
---
+++ backend/api/models/remark_reaction.py
@@ -0,0 +1,34 @@
+from django.db import models
+from api.models import Remark, User
+
+
+class RemarkReaction(models.Model):
+    REACTION_TYPES = (
+        ("like", "Like"),
+        ("dislike", "Dislike")
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="remark_reactions")
+    remark = models.ForeignKey(Remark, on_delete=models.CASCADE, related_name="remark_reactions")
+    reaction = models.CharField(max_length=8, choices=REACTION_TYPES, null=False, blank=False)
+    
+    @property
+    def user_id(self):
+        return self.user.pk
+    
+    @property
+    def username(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = "RemarkReaction"
+        verbose_name_plural = "RemarkReactions"
+        ordering = ("-updated_at",)
+        db_table = "remark_reaction"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "remark"],
+                name="remark_cant_be_liked_or_desliked_twice_by_same_user"
+            ),
+        ]
