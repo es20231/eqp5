3d4d2938d421818320e49b4449d3de67ddc88fee
acorreiacruz <antoniocruzjunior@outlook.com>
2023-08-15 07:25:20 -0300

    update: alterando o nome da permissão de IsReamarkReactionOwner para IsReactionOwner.

--- backend/api/permissions/is_reaction_owner.py
+++ backend/api/permissions/is_reaction_owner.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsRemarkReactionOwner(BasePermission):
+class IsReactionOwner(BasePermission):
     message = "The logged in user can only add or remove the likes or dislikes he has made"
 
     def has_object_permission(self, request, view, obj):
