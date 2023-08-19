a59cd8760868f1397f115387e33dfa4d10e73a7f
acorreiacruz <antoniocruzjunior@outlook.com>
2023-08-19 08:39:23 -0300

    update: removendo importações não utulizadas no sinal remove_like_or_dislike_remark.

--- backend/api/signals/remove_like_or_dislike_remark.py
+++ backend/api/signals/remove_like_or_dislike_remark.py
@@ -1,5 +1,5 @@
 from api.models import RemarkReaction
-from django.db.models.signals import post_save, pre_delete
+from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
 
