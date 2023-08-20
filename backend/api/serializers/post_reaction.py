a1c0215b4d3c5c04586987f1f33fbc51f86dc07b
acorreiacruz <antoniocruzjunior@outlook.com>
2023-08-19 22:38:15 -0300

    fix: consertando o PostReactionSerializer.

--- backend/api/serializers/post_reaction.py
+++ backend/api/serializers/post_reaction.py
@@ -5,5 +5,5 @@ from api.models import PostReaction
 class PostReactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostReaction
-        fields = ["created_at", "user", "username", "post", "reaction"]
-        read_only_fields = ["created_at", "user", "username", "post"]
+        fields = ["id", "created_at", "user", "username", "post", "reaction"]
+        read_only_fields = ["created_at", "user", "username"]
